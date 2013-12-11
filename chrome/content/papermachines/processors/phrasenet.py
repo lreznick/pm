032eb18768580fba01b96798871bc425f565cc3e
Myles Gurule <mylesgurule@gmail.com>
2013-12-10 21:55:30 -0500

    added split-into-intervals call in phrasenet; stores counts by time in edges, got rid of nodes

--- chrome/content/papermachines/processors/phrasenet.py
+++ chrome/content/papermachines/processors/phrasenet.py
@@ -11,7 +11,7 @@ import logging
 import traceback
 import codecs
 import textprocessor
-
+from collections import defaultdict
 
 class PhraseNet(textprocessor.TextProcessor):
 
@@ -23,30 +23,29 @@ class PhraseNet(textprocessor.TextProcessor):
     def _basic_params(self):
         self.name = 'phrasenet'
 
+    #add phrases w/ key by date that file was written
     def _findPhrases(self, pattern):
-        self.nodes = {}
-        self.edges = {}
-        for filename in self.files:
-            self.update_progress()
-            with codecs.open(filename, 'r', encoding='utf8') as f:
-                logging.info('processing ' + filename)
-                for re_match in pattern.finditer(f.read()):
-                    match = [w.lower() for w in re_match.groups()]
-                    if any([word in self.stopwords_set for word in
-                           match]):
-                        continue
-
-                    for word in match:
-                        if not word in self.nodes:
-                            self.nodes[word] = 1
-                        else:
-                            self.nodes[word] += 1
-
-                    edge = match[0] + self.edgesep + match[1]
-                    if not edge in self.edges:
-                        self.edges[edge] = 1
-                    else:
-                        self.edges[edge] += 1
+        #{x,y: {time: count})}
+        self.edges = defaultdict((lambda: defaultdict(lambda: 0)))
+        for time, fileset in self.labels.iteritems():
+            for filename in fileset:
+                self.update_progress()
+                with codecs.open(filename, 'r', encoding='utf8') as f:
+                    logging.info('processing ' + filename)
+                    for re_match in pattern.finditer(f.read()):
+                        match = [w.lower() for w in re_match.groups()]
+                        if any([word in self.stopwords_set for word in
+                               match]):
+                            continue
+                        """
+                        for word in match:
+                            if not word in self.nodes:
+                                nodes[word] = 1
+                            else:
+                                nodes[word] += 1
+                        """
+                        edge = match[0] + self.edgesep + match[1]
+                        self.edges[edge][time] += 1
 
     def process(self):
         logging.info('starting to process')
@@ -71,25 +70,38 @@ class PhraseNet(textprocessor.TextProcessor):
         logging.info('extracting phrases according to pattern '
                      + repr(pattern))
 
+        #should put files into intervals
+        self.split_into_intervals(start_and_end_dates=True)
+        #logging.info("intervals: " + str(self.labels))
+
         self._findPhrases(re.compile(pattern, flags=re.UNICODE))
+
+
 
         logging.info('generating JSON')
 
-        used_nodes = set()
+        #used_nodes = set()
+        
+        #jsondata = {'edges': []}
 
-        jsondata = {'nodes': [], 'edges': []}
+        #jsondata = self.edges
+        jsondata = filter((lambda (x, y): sum(y.values()) > 3),  self.edges.iteritems())
 
+        """
         top_edges = self.edges.keys()
-        top_edges.sort(key=lambda x: self.edges[x])
+        top_edges.sort(key=lambda x: sum(self.edges[x].values())
         top_edges.reverse()
         top_edges = top_edges[:50]
 
         for edge in top_edges:
             words = edge.split(',')
-            used_nodes.update(words)
+            #used_nodes.update(words)
 
-        nodeindex = dict(zip(used_nodes, range(len(used_nodes))))
+        #nodeindex = dict(zip(used_nodes, range(len(used_nodes))))
+        """
 
+
+        """
         for edge in top_edges:
             weight = self.edges[edge]
             words = edge.split(',')
@@ -99,9 +111,10 @@ class PhraseNet(textprocessor.TextProcessor):
         for node in used_nodes:
             jsondata['nodes'].append({'index': nodeindex[node],
                     'name': node, 'freq': self.nodes[node]})
-
+        """
         params = {'DATA': jsondata, 'PATTERN': pattern_str}
         self.write_html(params)
+        
 
 
 if __name__ == '__main__':
