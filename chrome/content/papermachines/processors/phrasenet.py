3969706b77c483ed82e00499b08b5df99ede3240
Myles Gurule <mylesgurule@gmail.com>
2013-12-13 13:46:21 -0500

    debugged tdidf, added times to html

--- chrome/content/papermachines/processors/phrasenet.py
+++ chrome/content/papermachines/processors/phrasenet.py
@@ -14,6 +14,7 @@ import textprocessor
 from collections import defaultdict
 from math import log
 import pdb
+from datetime import datetime, timedelta
 
 class PhraseNet(textprocessor.TextProcessor):
 
@@ -25,33 +26,67 @@ class PhraseNet(textprocessor.TextProcessor):
     def _basic_params(self):
         self.name = 'phrasenet'
 
+
+    def getfiledate(self, filename):
+        date_str = self.metadata[filename]['date']
+        if date_str.strip() == '':
+            logging.error(("File {:} has invalid date" +
+                          "-- removing...").format(filename))
+            del self.metadata[filename]
+            return None
+        cleaned_date = date_str[0:10]
+        if '-00' in cleaned_date:
+            cleaned_date = cleaned_date[0:4] + '-01-01'
+        try:
+            date_for_doc = datetime.strptime(cleaned_date,
+                    '%Y-%m-%d')
+            #datestr_to_datetime[date_str] = date_for_doc
+            # if (self.start_date is not None and 
+            #         date_for_doc < self.start_date):
+            #     logging.error(("File {:} is before date range" +
+            #                    "-- removing...").format(filename))
+            #     del self.metadata[filename]
+            #     return None
+            # if (self.end_date is not None and 
+            #         date_for_doc > self.end_date):
+            #     logging.error(("File {:} is after date range" +
+            #                    "-- removing...").format(filename))
+            #     del self.metadata[filename]
+            #     return None
+            return date_for_doc
+        except:
+            logging.error(traceback.format_exc())
+            logging.error('Date {:} not recognized'.format(cleaned_date))
+            return None
+
     #add phrases w/ key by date that file was written
     def _findPhrases(self, pattern):
         self.nodes = defaultdict((lambda: 0))
         self.times = defaultdict((lambda: defaultdict(lambda: 0)))
         self.edges = defaultdict((lambda: defaultdict(lambda: 0)))
-        for time, fileset in self.labels.iteritems():
-            for filename in fileset:
-                self.update_progress()
-                with codecs.open(filename, 'r', encoding='utf8') as f:
-                    logging.info('processing ' + filename)
-                    for re_match in pattern.finditer(f.read()):
-                        match = [w.lower() for w in re_match.groups()]
-                        if any([word in self.stopwords_set for word in
-                               match]):
-                            continue
-                        
-                        for word in match:
-                            self.nodes[word]+=1
-                        
-                        edge = match[0] + self.edgesep + match[1]
-                        self.times[time][edge] += 1
-                        self.edges[edge][time] += 1
+
+        for filename in self.files:
+            self.update_progress()
+            date = str(self.getfiledate(filename))
+            if (date is None): 
+                continue
+            with codecs.open(filename, 'r', encoding='utf8') as f:
+                logging.info('processing ' + filename)
+                for re_match in pattern.finditer(f.read()):
+                    match = [w.lower() for w in re_match.groups()]
+                    if any([word in self.stopwords_set for word in
+                           match]):
+                        continue
+                    for word in match:
+                        self.nodes[word]+=1
+                    
+                    edge = match[0] + self.edgesep + match[1]
+                    self.times[date][edge] += 1
+                    self.edges[edge][date] += 1
 
 
     #calculate tdidf scores per phrase in time period
     def _tfidf(self):
-        #pdb.set_trace()
         #tf for each phrase = count in time period * log (number of time periods / (1+number of time periods with phrase))
         self.tfidf_scores = defaultdict((lambda: defaultdict((lambda:0))))
         numperiods = float(len(self.times.keys()))
@@ -61,8 +96,7 @@ class PhraseNet(textprocessor.TextProcessor):
             for edge, count in edgecounts.iteritems():
                 num_matching_periods = 0
                 for x in self.times.values():
-                    val = x.get(edge) 
-                    if ((val!=None) and (val!=0)):
+                    if (x.__contains__(edge) ):
                         num_matching_periods+=1
                 self.tfidf_scores[edge][time] = (count * (log(numperiods/(1+num_matching_periods))))
 
@@ -71,6 +105,7 @@ class PhraseNet(textprocessor.TextProcessor):
     #    self._tfidf()
     #    for 
     def process(self):
+        #pdb.set_trace()
         logging.info('starting to process')
  
         self.stopwords_set = set(self.stopwords)
@@ -92,17 +127,9 @@ class PhraseNet(textprocessor.TextProcessor):
 
         logging.info('extracting phrases according to pattern '
                      + repr(pattern))
-        print("1")
-
-        #should put files into intervals
-        self.split_into_intervals(start_and_end_dates=False)
-        #logging.info("intervals: " + str(self.labels))
-        print("1")
 
         self._findPhrases(re.compile(pattern, flags=re.UNICODE))
-        print("1")
         self._tfidf()
-        print("1")
         logging.info('generating JSON')
 
         used_nodes = set()
@@ -114,9 +141,9 @@ class PhraseNet(textprocessor.TextProcessor):
 
         #logging.info("Edges: "+str(len(self.edges.iteritems())))
         
-        top_edges = self.edges.keys()
-        top_edges.sort(key=(lambda x: sum(self.edges[x].values())))
-        top_edges.reverse()
+        #top_edges = 
+        top_edges = sorted(self.edges.keys(), key=(lambda x: sum(self.edges[x].values())), reverse = True)
+        #top_edges.reverse()
         top_edges = top_edges[:50]
         logging.info("Top edge: "+str(top_edges))
 
@@ -138,6 +165,12 @@ class PhraseNet(textprocessor.TextProcessor):
         for node in used_nodes:
             jsondata['nodes'].append({'index': nodeindex[node],
                     'name': node, 'freq': self.nodes[node]})
+        #want a dict of {time:{edge:count}}, but just with some edges filtered out
+        filteredtimes = []
+        for time, edgecounts in self.times.iteritems():
+            filterededges = [{"edge":key, "count": value} for key, value in edgecounts.iteritems() if key in top_edges]
+            filteredtimes.append({"time": time, "edgecounts": filterededges})
+        jsondata['times'] = filteredtimes
         
         params = {'DATA': jsondata, 'PATTERN': pattern_str}
         self.write_html(params)
