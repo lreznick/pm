77c65b503970eeb9b7cfbf31ed97ad7129482be7
Myles Gurule <mylesgurule@gmail.com>
2013-12-15 19:18:09 -0500

    added area chart + brush with force updating

--- chrome/content/papermachines/processors/phrasenet.py
+++ chrome/content/papermachines/processors/phrasenet.py
@@ -11,7 +11,7 @@ import logging
 import traceback
 import codecs
 import textprocessor
-from collections import defaultdict
+from collections import defaultdict, Counter
 from math import log
 import pdb
 from datetime import datetime, timedelta
@@ -61,31 +61,35 @@ class PhraseNet(textprocessor.TextProcessor):
 
     #add phrases w/ key by date that file was written
     def _findPhrases(self, pattern):
-        self.nodes = defaultdict((lambda: 0))
-        self.times = defaultdict((lambda: defaultdict(lambda: 0)))
+        #self.nodes = defaultdict((lambda: 0))
+        self.times = defaultdict((lambda: 0))
         self.edges = defaultdict((lambda: defaultdict(lambda: 0)))
-
+        self.wordcounts = defaultdict((lambda:0))
         for filename in self.files:
             self.update_progress()
-            date = str(self.getfiledate(filename))
-            if (date is None): 
+            date = str(self.getfiledate(filename))[:10]
+            if ((date is None) or (date == "None")): 
                 continue
             with codecs.open(filename, 'r', encoding='utf8') as f:
                 logging.info('processing ' + filename)
-                for re_match in pattern.finditer(f.read()):
-                    match = [w.lower() for w in re_match.groups()]
-                    if any([word in self.stopwords_set for word in
-                           match]):
-                        continue
-                    for word in match:
-                        self.nodes[word]+=1
-                    
-                    edge = match[0] + self.edgesep + match[1]
-                    self.times[date][edge] += 1
-                    self.edges[edge][date] += 1
+                for line in f:
+                    self.wordcounts[date]+=len(line.split(' '))
+                    for re_match in pattern.finditer(line):
+                        match = [w.lower() for w in re_match.groups()]
+                        if any([word in self.stopwords_set for word in
+                               match]):
+                            continue
+                        #for word in match:
+                            #self.nodes[word]+=1
+                        
+                        edge = match[0] + self.edgesep + match[1]
+                        self.times[date] += 1
+                        self.edges[edge][date] += 1
+
 
 
     #calculate tdidf scores per phrase in time period
+    """
     def _tfidf(self):
         #tf for each phrase = count in time period * log (number of time periods / (1+number of time periods with phrase))
         self.tfidf_scores = defaultdict((lambda: defaultdict((lambda:0))))
@@ -99,7 +103,50 @@ class PhraseNet(textprocessor.TextProcessor):
                     if (x.__contains__(edge) ):
                         num_matching_periods+=1
                 self.tfidf_scores[edge][time] = (count * (log(numperiods/(1+num_matching_periods))))
+    
+    def _findTfIdfScores(self, scale=True):
+        self.freqs = Counter()
+        self.tf_by_doc = {}
+        self.max_tf = {}
+        self.df = Counter()
+        ngram = (1 if not hasattr(self, 'ngram') else self.ngram)
+        self.stemming = getattr(self, 'stemming', False)
+        for filename in self.files:
+            flen = 0
+            self.tf_by_doc[filename] = self.getNgrams(filename,
+                    n=ngram, stemming=self.stemming)
+            flen = sum(self.tf_by_doc[filename].values())
+            self.df.update(self.tf_by_doc[filename].keys())
 
+            self.freqs.update(self.tf_by_doc[filename])
+
+            for stem in self.tf_by_doc[filename].keys():
+                if scale:
+                    self.tf_by_doc[filename][stem] /= float(flen)  # max_tf_d
+                    this_tf = self.tf_by_doc[filename][stem]
+                else:
+                    this_tf = self.tf_by_doc[filename][stem] \
+                        / float(flen)
+
+                if stem not in self.max_tf or self.max_tf[stem] \
+                    < this_tf:
+                    self.max_tf[stem] = this_tf
+            self.update_progress()
+        n = float(len(self.files))
+        self.idf = dict((term, math.log10(n / df)) for (term, df) in
+                        self.df.iteritems())
+        self.tfidf = dict((term, self.max_tf[term] * self.idf[term])
+                          for term in self.max_tf.keys())
+        tfidf_values = self.tfidf.values()
+        top_terms = min(int(len(self.freqs.keys()) * 0.7), 5000)
+        min_score = sorted(tfidf_values, reverse=True)[min(top_terms,
+                len(tfidf_values) - 1)]
+        self.filtered_freqs = dict((term, freq) for (term, freq) in
+                                   self.freqs.iteritems()
+                                   if self.tfidf[term] > min_score
+                                   and self.df[term] > 3)
+
+    """
     #go through phrases and store significance
     #def _mww(self):
     #    self._tfidf()
@@ -129,12 +176,12 @@ class PhraseNet(textprocessor.TextProcessor):
                      + repr(pattern))
 
         self._findPhrases(re.compile(pattern, flags=re.UNICODE))
-        self._tfidf()
+        #self._findTfIdfScores()
         logging.info('generating JSON')
 
-        used_nodes = set()
+        #used_nodes = set()
         
-        jsondata = defaultdict((lambda: []))
+        jsondata = {}
 
         #jsondata = self.edges
         #top_edges = filter((lambda (x, y): sum(y.values()) > 3),  self.edges.iteritems())
@@ -142,36 +189,38 @@ class PhraseNet(textprocessor.TextProcessor):
         #logging.info("Edges: "+str(len(self.edges.iteritems())))
         
         #top_edges = 
-        top_edges = sorted(self.edges.keys(), key=(lambda x: sum(self.edges[x].values())), reverse = True)
+        #top_edges = sorted(self.edges.keys(), key=(lambda x: sum(self.edges[x].values())), reverse = True)
         #top_edges.reverse()
-        top_edges = top_edges[:50]
-        logging.info("Top edge: "+str(top_edges))
+        #top_edges = top_edges[:50]
+        #logging.info("Top edge: "+str(top_edges))
 
-        for edge in top_edges:
-            words = edge.split(',')
-            used_nodes.update(words)
+        #for edge in top_edges:
+        #    words = edge.split(',')
+        #    used_nodes.update(words)
 
-        nodeindex = dict(zip(used_nodes, range(len(used_nodes))))
+        #nodeindex = dict(zip(used_nodes, range(len(used_nodes))))
 
-        for edge in top_edges:
-            weight = sum(self.edges[edge].values())
-            timecounts = dict(filter((lambda (x, y): y>0), self.edges[edge].iteritems()))
-            tfidf = self.tfidf_scores[edge]
-            words = edge.split(',')
-            jsondata['edges'].append({'source': nodeindex[words[0]],
-                    'target': nodeindex[words[1]], 'weight': weight, 
-                    'weightbytime': timecounts, 'tfidf':tfidf})
+        #for edge in self.edges.iteritems():
+            #weight = sum(self.edges[edge].values())
+            #timecounts = dict(filter((lambda (x, y): y>0), self.edges[edge].iteritems()))
+            #tfidf = self.tfidf_scores[edge]
+            #words = edge.split(',')
+        #     jsondata['edges'].append({'source': nodeindex[words[0]],
+        #             'target': nodeindex[words[1]], 'weight': weight, 
+        #             'weightbytime': timecounts, 'tfidf':tfidf})
 
-        for node in used_nodes:
-            jsondata['nodes'].append({'index': nodeindex[node],
-                    'name': node, 'freq': self.nodes[node]})
-        #want a dict of {time:{edge:count}}, but just with some edges filtered out
-        filteredtimes = []
-        for time, edgecounts in self.times.iteritems():
-            filterededges = [{"edge":key, "count": value} for key, value in edgecounts.iteritems() if key in top_edges]
-            filteredtimes.append({"time": time, "edgecounts": filterededges})
-        jsondata['times'] = filteredtimes
-        
+        # for node in used_nodes:
+        #     jsondata['nodes'].append({'index': nodeindex[node],
+        #             'name': node, 'freq': self.nodes[node]})
+        # #want a dict of {time:{edge:count}}, but just with some edges filtered out
+        # filteredtimes = []
+        # for time, edgecounts in self.times.iteritems():
+        #     filterededges = [{"edge":key, "count": value} for key, value in edgecounts.iteritems() if key in top_edges]
+        #     if ((len(filterededges)>0) and (time is not None)):
+        #         filteredtimes.append({"time": time, "edgecounts": filterededges})
+        # jsondata['times'] = filteredtimes
+        jsondata['edges'] = [{'p':x, 't':y} for x,y in self.edges.iteritems()]
+        jsondata['times'] = [{'t':x, 'c':y} for x,y in self.times.iteritems()]
         params = {'DATA': jsondata, 'PATTERN': pattern_str}
         self.write_html(params)
         
--- chrome/content/papermachines/processors/support/phrasenet.js
+++ chrome/content/papermachines/processors/support/phrasenet.js
@@ -1,40 +1,46 @@
 var pattern = data["PATTERN"];
-var nodedata = data["DATA"]['nodes'];
+var numnodes = 50;
+d3.select("body").append("h1").text(pattern);
+var fontSize = d3.scale.log().range([10,32]);
+var linkSize = d3.scale.linear().range([2,10]);
+
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+var timedata = data["DATA"]['times'].sort(function(a, b){
+    return parseDate(a.t)-parseDate(b.t); 
+});
 var edgedata = data["DATA"]['edges'];
+
+var margin = {top: 10, right: 10, bottom: 100, left: 40},
+    width = 960 - margin.left - margin.right,
+    pheight = 800,
+    margin2 = {top: 10+pheight, right: 10, bottom: 20, left: 40},
+    cheight = 500;
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", pheight + cheight + margin.top + margin.bottom);
+
+
+var nodes=[];
+var edges=[];
 
 var fontSize = d3.scale.log().range([10,32]);
 var linkSize = d3.scale.linear().range([2,10]);
 
-var vals = nodedata.map(function (d) { return +d.freq});
-fontSize.domain([d3.min(vals), d3.max(vals)]);
-
-vals = edgedata.map(function (d) { return +d.weight});
-linkSize.domain([d3.min(vals), d3.max(vals)]);
-
 var diagonal = d3.svg.diagonal()
   .source(function (d) {
-    var b_s = d3.select("#node" + d.source.index.toString())[0][0].getBBox(),
-        b_t = d3.select("#node" + d.target.index.toString())[0][0].getBBox();
+    var b_s = d3.select("#node" + d.source.name)[0][0].getBBox(),
+        b_t = d3.select("#node" + d.target.name)[0][0].getBBox();
 
     return {'x': d.source.x, 'y': d.source.y + (d.source.y < d.target.y ? b_s.height * 0.5 : b_s.height * -0.5)};
 
   })
   .target(function (d) {
-    var b_s = d3.select("#node" + d.source.index.toString())[0][0].getBBox(),
-        b_t = d3.select("#node" + d.target.index.toString())[0][0].getBBox();
+    var b_s = d3.select("#node" + d.source.name)[0][0].getBBox(),
+        b_t = d3.select("#node" + d.target.name)[0][0].getBBox();
 
     return {'x': d.target.x, 'y': d.target.y + (d.source.y < d.target.y ? b_t.height * -0.5 : b_t.height * 0.5)};
-
-    // return {'x': d.target.x, 'y': d.target.y - (d.source.y < d.target.y ? textOffset : -textOffset)};
   });
-
-var width = 1024, height = 768;
-
-d3.select("body").append("h1").text(pattern);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
 svg.append("svg:defs").selectAll("marker")
     .data(["defaultmarker"])
@@ -50,55 +56,13 @@ svg.append("svg:defs").selectAll("marker")
   .append("svg:path")
     .attr("d", "M0,0L10,5L0,10z");
 
-var force = d3.layout.force().size([width, height])
+var force = d3.layout.force().size([width, pheight])
     .distance(100)
     .charge(-300)
-    .gravity(0.15)
-    .nodes(nodedata)
-    .links(edgedata)
-    .start();
-
-var node = svg.selectAll(".node")
-    .data(nodedata)
-  .enter().append("svg:g")
-    .attr("class", "node")
-    .call(force.drag);
-
-node.append("text")
-    .attr("dx", 0)
-    .attr("dy", ".35em")
-    .style("font-size", function (d) { return fontSize(+d.freq); })
-    .attr("id", function (d, i) { return "node" + i.toString();})
-    .text(function(d) { return d.name; });
-
-var link = svg.selectAll(".link")
-    .data(edgedata)
-  .enter().append("path")
-    .attr("class", "link")
-    .style("stroke-width", function (d) { return linkSize(+d.weight);})
-    .style("stroke", "#000")
-    .attr("d", diagonal)
-    .attr("marker-end", "url(#defaultmarker)");
-
-
-force.on("tick", function() {
-  link.attr("d", diagonal);
-
-  // var q = d3.geom.quadtree(nodedata),
-  //   i = 0,
-  //   n = nodedata.length;
-  // while (++i < n) {
-  //   q.visit(collide(nodedata[i]));
-  // }
-
-  node.attr("transform", function(d) {
-    d.x = Math.max(fontSize(+d.freq), Math.min(d.x, width - fontSize(+d.freq)));
-    d.y = Math.max(fontSize(+d.freq), Math.min(d.y, height - fontSize(+d.freq)));
-  return "translate(" + d.x + "," + d.y + ")"; });
-});
+    .gravity(0.15);
 
 function collide(d) {
-  var b = d3.select("#node" + d.index.toString())[0][0].getBBox(),
+  var b = d3.select("#node" + d.name.toString())[0][0].getBBox(),
     nx1 = b.x,
     nx2 = b.x + b.width,
     ny1 = b.y,
@@ -106,7 +70,7 @@ function collide(d) {
 
   return function (quad, x1, y1, x2, y2) {
     if (quad.point && (quad.point !== d)) {
-      var b2 = d3.select("#node" + quad.point.index.toString())[0][0].getBBox(),
+      var b2 = d3.select("#node" + quad.point.name.toString())[0][0].getBBox(),
         ox1 = b2.x,
         ox2 = b2.x + b2.width,
         oy1 = b2.y,
@@ -128,3 +92,185 @@ function collide(d) {
     return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
   };
 }
+
+
+var x = d3.time.scale()
+    .range([0, width])
+    .domain(d3.extent(timedata, function(d) { return parseDate(d.t); }));
+
+var y = d3.scale.linear()
+    .range([cheight, 0])
+    .domain([0, d3.max(timedata, function(d) { return d.c; })]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var brush = d3.svg.brush()
+    .x(x)
+    .on("brushend", brushed);
+
+var area = d3.svg.area()
+    .x(function(d) {return x(parseDate(d.t));})
+    .y0(function(d) {return y(0);})
+    .y1(function(d) {return y(d.c); });
+
+var context = svg.append("g")
+  .attr("class", "context")
+  .append("g")
+  .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+  context.append("path")
+      .datum(timedata)
+      .attr("class", "area")
+      .attr("d", area);
+
+  context.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + cheight + ")")
+      .call(xAxis);
+
+ context.append("g")
+      .attr("class", "x brush")
+      .call(brush)
+    .selectAll("rect")
+      .attr("y", -6)
+      .attr("height", cheight + 7);
+
+  context.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Count");
+
+function brushed() {
+  var period = brush.extent();
+  if (!brush.empty()) updatePhrases(dunning(period[0], period[1]), 0, function(d){return d[1];});
+  return true;
+}
+
+function dunning(p1, p2){
+  var i = 0;
+  var dvals = [];
+  for (i = 0; i<edgedata.length; i++){
+    var d = edgedata[i];
+    var edge = d.p;
+    var times = d.t;
+    if (Object.keys(times).length>1){
+    var o = 0;
+    var e = 0;
+    for (var t in times){
+      var count = times[t];
+      var time = parseDate(t);
+      if ((time<p2)&&(time>p1)) o+=count
+      else e+=count;
+    }
+    if ((o>0)&&(e>0)) {
+      dvals.push([edge, (o*(Math.log(o/e)))]);
+    }
+  }
+  }
+  return dvals;
+}
+//add datum to array, if necessary; return index of datum in array
+function getorInsertNode(arr, datum, weight){
+  var i;
+  for (i = 0; i<arr.length; i++){
+    if (arr[i].name == datum) {
+      arr[i].weight += weight;
+      return i;
+    }
+  }
+  var i = arr.push({'name':datum, 'weight':weight});
+  return i-1;
+}
+
+//given array of ["source,target", weight]
+function getData(d, accessor, f){
+  var i;
+  d.sort(function(a, b){return f(b)-f(a);});
+  var stop = Math.min(numnodes, d.length-1);
+  for(i=0; i<stop; i++){
+    var edge = d[i][accessor];
+    var weight = f(d[i]);
+    var s =  edge.split(","); var source = s[0]; var target = s[1];
+    var si = getorInsertNode(nodes, source, weight);
+    var ti = getorInsertNode(nodes, target, weight);
+    edges.push({'source': si, 'target': ti, 'weight': weight, 'id': edge});
+  }
+
+}
+
+function updatePhrases(d, a, f){
+  nodes = [];
+  edges = [];
+  getData(d, a, f);
+  console.log(nodes);
+  var vals = nodes.map(function (d) { return d.weight});
+  var e = d3.extent(vals); console.log(e);
+  if (e[0] <= 0) e[0] = 1;
+  fontSize = d3.scale.log().clamp(true).range([10,32]).domain(e);
+
+  vals = edges.map(function (d) { return d.weight});
+  linkSize = linkSize.copy().domain(d3.extent(vals));
+  
+
+  force.nodes(nodes)
+      .links(edges)
+      .start();
+
+  var node = svg.selectAll(".node")
+      .data(nodes, function(d){return d.name;});
+
+  var nodeEnter = node.enter().append("svg:g")
+      .attr("class", "node")
+      .call(force.drag);
+
+  nodeEnter.append("text")
+      .attr("dx", 0)
+      .attr("dy", ".35em")
+      .style("font-size", function (d) { return fontSize(d.weight); })
+      .attr("id", function (d, i) { return "node" + d.name;})
+      .text(function(d) { return d.name; });
+
+  node.exit().transition(3000).style("color", "red").remove();
+
+  var link = svg.selectAll(".link")
+      .data(edges, function(d){d.id});
+
+  var linkEnter = link.enter().append("path")
+      .attr("class", "link")
+      .style("stroke-width", function (d) { return linkSize(d.weight);})
+      .style("stroke", "#000")
+      .attr("d", diagonal)
+      .attr("marker-end", "url(#defaultmarker)");
+
+  link.exit().transition(3000).style("stroke", "red").remove();
+
+force.on("tick", function() {
+  link.attr("d", diagonal);
+
+  node.attr("transform", function(d) {
+    d.x = Math.max(fontSize(d.weight), Math.min(d.x, width - fontSize(d.weight)));
+    d.y = Math.max(fontSize(d.weight), Math.min(d.y, pheight - fontSize(d.weight)));
+  return "translate(" + d.x + "," + d.y + ")"; });
+});
+
+}
+
+updatePhrases(edgedata, "p", function(d){
+  var t = d.t;
+  var sum = 0;
+  for (var key in t){
+    sum += t[key];
+  }
+  return sum;
+});
